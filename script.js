b536354d72cf1e4cb022094198e7bdfbd4f5c624
remchi301 <umigremory301@gmail.com>
2025-05-23 06:29:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ burger.addEventListener('click', () => {
 // Toggle light/dark mode
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
-  themeToggle.textContent = document.body.classList.contains('dark-mode') ? '☀︎' : '❨';
+  themeToggle.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
 });
 
 // Hide loader when page fully loads
